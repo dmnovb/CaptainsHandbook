7b4e16e17017b37b9b1fe7937ccbf3fff3f8aaa6
bobi <boqn.damqnov03@gmail.com>
2023-03-27 22:11:19 +0300

    filename change

---
+++ src/data.ts
@@ -0,0 +1,53 @@
+export const classes = [
+    {
+        title: "The Captain",
+        responsibility: `The captain is the leader of the crew. He can assume the roles of all other management personnel, 
+        fulfilling their functions and possessing their privileges. 
+        He has the final say in all decisions. Orders given directly from the captain are mandatory. 
+        The captain is the only person who can helm the ship (exceptions apply to emergency situations, 
+        or if the captain gives permission). 
+        Any disputes that may arise are settled by the captain. His word is final. 
+        Both the head of security and the storage logistician report to the captain. `
+    },
+    {
+        title: "The Head Of Security",
+        responsibility: `The head of security is responsible for keeping the peace 
+        aboard the submarine. 
+        He can arrest anyone at any time. He’s also 
+        responsible for dealing with any outside threats. 
+        He’s the only person (along with the captain) 
+        who can give crewmembers permission to use the canons and turrets. 
+        Only he can authorize crewmembers to use weapons. After each mission, 
+        crewmembers must report the amount of ammo they’ve used to the Head of security, 
+        who must report the total amount of used ammunition to the storage logistician.  
+        If there are any other security members onboard, they follow the command of the Head of security. 
+        The head of security may make a member of another department an honorary gunner. 
+        Honorary gunners are non-security officers who have the constant permission to use the turrets. 
+        In cases of emergency, any crewmember may use the turrets. The head of security gives direct orders to the members of the security department 
+        on how to fulfill their duties, 
+        however The head of security must comply with the orders of the captain.`
+    },
+    {
+        title: "The Storage Logistician",
+        responsibility: `The storage logistician is responsible for the resources aboard the ship. 
+        He’s the one in charge of the storage: before a crewmember takes anything from there, 
+        they must consult with him. 
+        He’s the only one who can authorize the crafting of items. 
+        If any crewmember notices a shortage of any consumable item (oxygen tanks, welding fuel etc.), 
+        he must report it to the Storage logistician. 
+        Crafting requests may also be submitted to him, he has the power to authorize them. 
+        The storage logistician is also the head of the engineering department (both mechanics and engineers fall under this umbrella). 
+        All members of the engineering department must follow the orders of the storage logistician. 
+        The storage logistician is authorized to carry a weapon and use the turrets, 
+        but is not obliged to follow the orders of the Head of security.  
+        The storage logistician must comply with the orders of the captain.`
+    },
+    {
+        title: "The Council",
+        responsibility: `Either the storage logistician or the head of security may challenge a captain’s order, 
+        or propose an idea. In such a case, a vote is held to determine whether the proposition will go through 
+        or no. The captain’s vote has an index of 1.5 , 
+        whereas the individual vote of the head of security and the storage logistician is 1.`
+    }
+    
+]

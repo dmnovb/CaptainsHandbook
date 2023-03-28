be45aaf461c1beebe108f673dd2f9b6db054fcf4
boyan damyanov <boyan@boyans-MacBook-Air.local>
2023-03-28 10:29:57 +0300

    themeing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,39 @@
+import type { AppProps } from "next/app";
+import { red } from '@mui/material/colors';
+import {ThemeProvider, createTheme} from "@mui/material/styles/";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = createTheme({
     palette: {
       primary: {
-        main: "#03a9f4",
+        main: red[500],
       },
       secondary: {
-        main: "#03a9f4",
+        main: red[100]
       },
       background: {
-        default: "#20c34",
-      },
+        default: "#000000"
+      }
     },
     typography: {
       h6: {
         fontSize: "1rem",
         fontWeight: 100,
+        color: '#ffffff'
       },
       h5: {
         fontSize: "1.9rem",
         fontFamily: "roboto",
+        color: '#ffffff'
       },
       h4: {
         fontSize: "2rem",
+        color: '#ffffff'
       },
+      h3: {
+      color: '#ffffff'
+      }
     },
   });
 
--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
+import CssBaseline  from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
-import { classes } from "@/types";
+import { classes } from "@/data";
 
 type Personnel = {
   title: string;
@@ -15,15 +16,36 @@ const Content = () => {
     setWorkers(classes);
   }, []);
 
+  return (
+    <CssBaseline>
+      <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        background: 'background.default'
+      }}
+    >
+      <Typography variant="h3">Captain&#39;s Handbook</Typography>
+      <Typography variant="h6" sx={{ fontStyle: "italic" }}>
+        to my beloved son
       </Typography>
       <Box sx={{ mt: "3rem" }}>
         <Typography variant="h5" sx={{ textDecoration: "underline" }}>
           Leadership and Authority.
         </Typography>
       </Box>
-
+      <Box sx={{ textAlign: "center", padding: "4rem" }}>
+        {workers.map((person: Personnel, i: number) => (
+          <Box sx={{ marginTop: "2rem", padding: "2rem" }} key={i}>
+            <Typography variant="h4">{person.title}</Typography>
+            <Typography variant="h6">{person.responsibility}</Typography>
+          </Box>
+        ))}
       </Box>
     </Box>
+    </CssBaseline>
+    
   );
 };
 
